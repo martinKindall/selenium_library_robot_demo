08e37ab800dd773bfcd3cd869f148650fd1f3a86
martinKindall <mart256@gmail.com>
2021-03-13 14:19:17 +0100

    Adding small logic to show and hide error message depending on entering empty username or password.

--- static/main.js
+++ static/main.js
@@ -1,4 +1,45 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    hideCredentialsCannotBeEmpty();
+
+    const userCredentialsStore = {};
+
+    function getUsernameAndPassword() {
+        const username = document.getElementById('input_username').value;
+        const password = document.getElementById('input_password').value;
+
+        return {
+            'username': username,
+            'password': password
+        };
+    }
+
+    function validateCredentials(userCredentials) {
+        if (!userCredentials.username || !userCredentials.password) {
+            showCredentialsCannotBeEmpty();
+        } else {
+            hideCredentialsCannotBeEmpty();
+        }
+    }
+
+    function hideCredentialsCannotBeEmpty() {
+        document.getElementById('error_msg').style.display = 'none';
+    }
+
+    function showCredentialsCannotBeEmpty() {
+        document.getElementById('error_msg').style.display = 'block';
+    }
+
+    document.getElementById('login-button').onclick = function(){
+          const userAndPass = getUsernameAndPassword()
+          validateCredentials(userAndPass);
+          console.log(userAndPass);
+    }
+
+    document.getElementById('sign-in-button').onclick = function(){
+          const userAndPass = getUsernameAndPassword()
+          validateCredentials(userAndPass);
+          console.log(userAndPass);
+    }
 
 });
